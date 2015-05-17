09482f645f5f58deeca341b19c4fada768d5cc1b
Antti Sykäri <antti.sykari@gmail.com>
2015-05-17 12:29:39 +0300

    Get cycle.js from bower; add babel support; copy & paste the hello world example
    It works!

--- app/main.js
+++ app/main.js
@@ -1,5 +1,23 @@
+function run() {
+    let { h } = Cycle;
+
+    function computer(interactions) {
+        return interactions.get('.myinput', 'input')
+            .map(ev => ev.target.value)
+            .startWith('')
+            .map(name =>
+                h('div', [
+                    h('label', 'Name:'),
+                    h('input.myinput', { attributes: { type: 'text' } }),
+                    h('hr'),
+                    h('h1', 'Hello ' + name)
+                ])
+        );
+    }
+
+    Cycle.applyToDOM('.js-container', computer);
+}
+
 module.exports = {
-	run: function() {
-		console.log('hello');
-	}
+	run: run
 };
